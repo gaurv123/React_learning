e7d957bce9e45eeab919afd16a1c7b98078e562c
gaurav singh <thakurgaurav273@gmail.com>
2023-02-24 20:59:22 +0530

    added darkmode in navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 // import logo from '../public/name.txt';
+import React ,{useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [theme,setTheme]=useState('light');
+  const [font,setFont]=useState('black');
+  const changeTheme=()=>{
+    if(theme==="light"){
+      setTheme("dark");
+      setFont("white");
+    }
+    else{
+      setTheme("light");
+      setFont("black");
+    }
+  }
   return (
-    <Navbar tittle="ConvertTextApp" link1="Home"/>
+    <Navbar tittle="ConvertTextApp" link1='home' apptheme={changeTheme} mode={theme} fontcolor={font}/>
   );
 }
-
 
 export default App;
--- src/components/form.js
+++ src/components/form.js
@@ -7,9 +7,8 @@ export default function Form(props) {
   const textModified=(event)=>{
     console.log("text area selected");
     setText(event.target.value);
-  
   };
-  
+
   const UpdateText=()=>{
     let new_text=text.toUpperCase();
     setText(new_text);
@@ -20,16 +19,20 @@ export default function Form(props) {
   };
   const num=text.split(" ").length;
   const num2=text.length;
+
+
   return (
     <>
     <div className='form'>
       <h1>{props.title}</h1>
-        <textarea rows='10' cols='40' onChange={textModified} value={text} />
+        <textarea rows='10' cols='40' onChange={textModified} value={text} /> <br/>
+        
 
          <br/>
         <button className='btn btn-primary' onClick={UpdateText}>ToUpperCase</button> &nbsp;
         <button className='btn btn-primary' onClick={UpdateToLower}>ToLowerCase</button>
    
+  
     <h6>{num} words</h6>
     <h6>{num2} characters</h6>
     </div>
